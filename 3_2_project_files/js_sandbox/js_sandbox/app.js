ede4e9f16d0d003691cc745760163d37baf41676
joelszn <duranjoel.a@gmail.com>
2019-10-03 22:22:58 -0400

    query selector all

--- 3_2_project_files/js_sandbox/js_sandbox/app.js
+++ 3_2_project_files/js_sandbox/js_sandbox/app.js
@@ -24,66 +24,80 @@
 
 // document.querySelector()
 
-console.log(document.querySelector('#task-title'));
-console.log(document.querySelector('.card-title'));
-console.log(document.querySelector('h5'));
+// console.log(document.querySelector('#task-title'));
+// console.log(document.querySelector('.card-title'));
+// console.log(document.querySelector('h5'));
 
-document.querySelector('li').style.color = 'red';
-document.querySelector('ul li').style.color = 'blue';
+// document.querySelector('li').style.color = 'red';
+// document.querySelector('ul li').style.color = 'blue';
 
-// CSS3 lets you pick nth child from a list
-document.querySelector('li:last-child').style.color = 'red';
-document.querySelector('li:nth-child(3)').style.color = 'yellow';
-document.querySelector('li:nth-child(4)').textContent = 'I changed this';
-document.querySelector('li:nth-child(odd)').style.background = '#ccc';
-document.querySelector('li:nth-child(even)').style.background = '#f4f4f4';
-
-
+// // CSS3 lets you pick nth child from a list
+// document.querySelector('li:last-child').style.color = 'red';
+// document.querySelector('li:nth-child(3)').style.color = 'yellow';
+// document.querySelector('li:nth-child(4)').textContent = 'I changed this';
+// document.querySelector('li:nth-child(odd)').style.background = '#ccc';
+// document.querySelector('li:nth-child(even)').style.background = '#f4f4f4';
 
 
 
+//  document.getElementsByClassName
+// const items = document.getElementsByClassName('collection-item');
+// console.log(items);
+// console.log(items[0]);
+// items[0].style.color = 'red';
+// items[3].textContent = 'red';
+
+// const listItems = document.querySelector('ul').getElementsByClassName('collection-item');
+// console.log(listItems);
+
+
+
+
+// document.getElementbyTagName same thing just selecting by the tag name
+// let lis = document.getElementsByTagName('li');
+// console.log(lis);
+// console.log(lis[0]);
+// lis[0].style.color = 'red';
+// lis[3].textContent = 'word';
+
+// // Convert HTML collection into array
+// lis = Array.from(lis);
+
+// lis.reverse();
+
+// // lis.forEach(function)
+// console.log(lis);
+
+
+// QUERY SELECTOR ALL
+
+const items = document.querySelectorAll('ul.collection.li.collection-item');
+
+items.forEach(function(item, index){
+    item.textContent = `${index}: Hello`;
+    
+});
+
+const liOdd = document.querySelectorAll('li.nth-child(odd)');
+const liEven = document.querySelectorAll('li.nth-child(even)');
+
+for(let i = 0; i < liEven.length; i++) {
+    liEven[i].style.background = '#f4f';
+}
+
+
+console.log(items);
 
 
 
 
 
-// let val;
 
-// val = document;
-// val = document.all;
-// val = document.all[2];
-// val = document.all.length;
-// val = document.head;
-// val = document.body;
-// val = document.doctype;
-// val = document.domain;
-// val = document.URL;
-// val = document.characterSet;
-// val = document.contentType;
 
-// val = document.forms;
-// val = document.forms[0];
-// val = document.forms[0].id;
-// val = document.forms[0].method;
-// val = document.forms[0].action;
 
-// val = document.links;
-// val = document.links[0];
-// val = document.links[0].id;
-// val = document.links[0].className;
-// val = document.links[0].classList[0];
 
-// val = document.images;
 
-// val = document.scripts;
-// val = document.scripts[2].getAttribute('src');
 
-// let scripts = document.scripts;
 
-// let scriptsArr = Array.from(scripts);
 
-// scriptsArr.forEach(function(script) {
-//   console.log(script.getAttribute('src'));
-// });
 
-// console.log(val);
