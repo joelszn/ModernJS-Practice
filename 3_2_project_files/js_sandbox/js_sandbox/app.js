bcad91cf9be3d981df5fcac896ce4b7166186449
joelszn <duranjoel.a@gmail.com>
2019-10-06 14:34:11 -0400

    getting DOM elements by tag name

--- 3_2_project_files/js_sandbox/js_sandbox/app.js
+++ 3_2_project_files/js_sandbox/js_sandbox/app.js
@@ -71,24 +71,48 @@
 
 // QUERY SELECTOR ALL
 
-const items = document.querySelectorAll('ul.collection.li.collection-item');
+// const items = document.querySelectorAll('ul.collection.li.collection-item');
 
-items.forEach(function(item, index){
-    item.textContent = `${index}: Hello`;
+// items.forEach(function(item, index){
+//     item.textContent = `${index}: Hello`;
     
-});
+// });
 
-const liOdd = document.querySelectorAll('li.nth-child(odd)');
-const liEven = document.querySelectorAll('li.nth-child(even)');
+// const liOdd = document.querySelectorAll('li.nth-child(odd)');
+// const liEven = document.querySelectorAll('li.nth-child(even)');
 
-for(let i = 0; i < liEven.length; i++) {
-    liEven[i].style.background = '#f4f';
-}
-
-
-console.log(items);
+// for(let i = 0; i < liEven.length; i++) {
+//     liEven[i].style.background = '#f4f';
+// }
 
 
+// console.log(items);
+
+
+
+// GET ELEMENTS BY CLASS NAME
+
+// const items = document.getElementsByClassName('collection-item');
+// console.log(items);
+// console.log(items[0]);
+// items[0].style.color = 'red';
+
+// const listItems = document.querySelector('ul').getElementsByClassName('collection-item');
+// console.log(listItems);
+
+// GET ELEMENTS BY TAGNAME
+let lis = document.getElementsByTagName('li');
+console.log(lis);
+
+console.log(lis[4]);
+lis[0].style.color = 'red';
+lis[3].textContent = 'Test';
+
+// Convert HTML Collection into array
+lis = Array.from(lis);
+
+lis.reverse(); // this alone will give you an error because html collection is not an arr
+console.log(lis);
 
 
 
